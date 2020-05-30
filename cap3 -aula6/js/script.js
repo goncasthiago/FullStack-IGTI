35f9446a62d63fa1fd383878dc6fdc7ecdf29561
goncasthiago <thiagodebia@gmail.com>
2020-05-30 13:06:44 -0300

    CRUD Completo

--- cap3 -aula6/js/script.js
+++ cap3 -aula6/js/script.js
@@ -2,54 +2,105 @@
 
 window.addEventListener('load', start);
 
-var globalNames = ['Um', 'Dois', 'Três', 'Quatro'];
+var globalNames = [ 'Um', 'Dois', 'Três', 'Quatro' ];
 var inputName = null;
+var isEditing = false;
+var currentIndex = null;
 
-function start(){
-    preventFormSubmit();
-    inputName = document.querySelector('#inputName');
-    activateInput();
-    render();
+function start() {
+	preventFormSubmit();
+	inputName = document.querySelector('#inputName');
+	activateInput();
+	render();
 }
 
-function preventFormSubmit(){
-    function handleFormSubmit(event){
-        event.preventDefault();
-    }
-    var form = document.querySelector('form');
-    form.addEventListener('submit', handleFormSubmit);
+function preventFormSubmit() {
+	function handleFormSubmit(event) {
+		event.preventDefault();
+	}
+	var form = document.querySelector('form');
+	form.addEventListener('submit', handleFormSubmit);
 }
 
-function activateInput(){
+function activateInput() {
+	function insertName(newName) {
+		globalNames.push(newName);
+		render();
+	}
 
+	function updateName(newName) {
+		globalNames[currentIndex] = newName;
+		render();
+	}
+	function handleTyping(event) {
+		var hasText = !!event.target.value && event.target.value.trim() !== '';
 
-function handleTyping(event){
-    function insertName(newName){
-        globalNames.push(newName);
-        render();
+		if (!hasText) {
+			event.target.value = '';
+			return;
+		}
+		if (event.key === 'Enter') {
+			if (isEditing) {
+				updateName(event.target.value.trim());
+			} else {
+				insertName(event.target.value.trim());
+			}
+			isEditing = false;
+			clearInput();
+		}
+	}
 
-    }
-
-    if (event.key === 'Enter'){
-        insertName(event.target.value);
-    }
+	inputName.addEventListener('keyup', handleTyping);
+	inputName.focus();
 }
 
-    inputName.focus();
-    inputName.addEventListener('keyup', handleTyping);
+function render() {
+	function createDeleteButton(index) {
+		function deleteName() {
+			globalNames.splice(index, 1);
+			render();
+		}
+
+		var button = document.createElement('button');
+		button.classList.add('deleteButton');
+		button.textContent = 'x';
+		button.addEventListener('click', deleteName);
+		return button;
+	}
+
+	function createSpan(name, index) {
+		function editItem() {
+			inputName.value = name;
+			inputName.focus();
+			isEditing = true;
+			currentIndex = index;
+		}
+		var span = document.createElement('span');
+		span.classList.add('clickable');
+		span.textContent = name;
+
+		span.addEventListener('click', editItem);
+		return span;
+	}
+
+	var divNames = document.querySelector('#names');
+	divNames.innerHTML = '';
+	var ul = document.createElement('ul');
+
+	for (var i = 0; i < globalNames.length; i++) {
+		var currentName = globalNames[i];
+		var li = document.createElement('li');
+		var button = createDeleteButton(i);
+		var span = createSpan(currentName, i);
+		li.appendChild(button);
+		li.appendChild(span);
+		ul.appendChild(li);
+	}
+	divNames.appendChild(ul);
+	clearInput();
 }
 
-function render(){
-    var divNames = document.querySelector('#names');
-    var ul = document.createElement('ul');
-
-    for (var i = 0; i < globalNames.length; i++){
-        var li = document.createElement('li');
-        var currentName = globalNames[i];
-        li.textContent = currentName;
-        ul.appendChild(li);
-
-    }
-    divNames.appendChild(ul);
-
+function clearInput() {
+	inputName.value = '';
+	inputName.focus();
 }
